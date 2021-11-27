3ea6bc5371800da3e9c486720b542bee65fc959d
ross-weir <29697678+ross-weir@users.noreply.github.com>
2021-11-27 22:01:23 +1100

    fix popover tooltip

--- src/components/WalletDetailsForm/WalletDetailsForm.stories.tsx
+++ src/components/WalletDetailsForm/WalletDetailsForm.stories.tsx
@@ -7,17 +7,14 @@ import WalletDetailsForm from '.';
 export default {
   title: 'Wallet details form',
   component: WalletDetailsForm,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as ComponentMeta<typeof WalletDetailsForm>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof WalletDetailsForm> = (args) => (
   <WalletDetailsForm {...args} />
 );
 
 export const WalletDetailsCompleted = Template.bind({});
 
-WalletDetailsCompleted.args = { buttonText: 'Create' };
+WalletDetailsCompleted.args = {
+  buttonText: 'Create',
+};
--- src/components/WalletDetailsForm/index.tsx
+++ src/components/WalletDetailsForm/index.tsx
@@ -4,7 +4,7 @@ import Stack from '@kiwicom/orbit-components/lib/Stack';
 import Heading from '@kiwicom/orbit-components/lib/Heading';
 import InputField from '@kiwicom/orbit-components/lib/InputField';
 
-export interface WalletDetailsFormProps {
+interface WalletDetailsFormProps {
   buttonText: string;
   onSubmit?: (form: { name: string; password: string }) => void;
 }
@@ -13,20 +13,22 @@ type FieldName = 'name' | 'password1' | 'password2';
 type InputState = Record<FieldName, string>;
 type ErrorState = Record<FieldName, string>;
 
+const initialState = {
+  name: '',
+  password1: '',
+  password2: '',
+};
+
 function WalletDetailsForm({ onSubmit, buttonText }: WalletDetailsFormProps) {
-  const [input, setInput] = useState<InputState>({
-    name: '',
-    password1: '',
-    password2: '',
-  });
-  const [error, setError] = useState<ErrorState>({
-    name: '',
-    password1: '',
-    password2: '',
-  });
+  const [input, setInput] = useState<InputState>({ ...initialState });
+  const [error, setError] = useState<ErrorState>({ ...initialState });
+
+  const clearErrors = () => setError(() => ({ ...initialState }));
 
   const onInputChange = (e) => {
     const { name, value } = e.target;
+
+    clearErrors();
 
     setInput((prev) => ({
       ...prev,
@@ -40,16 +42,23 @@ function WalletDetailsForm({ onSubmit, buttonText }: WalletDetailsFormProps) {
     if (!name) {
       setError((prev) => ({
         ...prev,
-        [name]: 'Must set a wallet name',
+        name: 'Must set a wallet name',
       }));
 
       return false;
     }
 
-    if (password1 && password1 !== password2) {
+    if (!password1 && !password2) {
       setError((prev) => ({
         ...prev,
-        [password1]: "Passwords don't match",
+        password1: 'Must set a password',
+      }));
+    }
+
+    if (password1 !== password2) {
+      setError((prev) => ({
+        ...prev,
+        password1: "Passwords don't match",
       }));
 
       return false;
@@ -73,7 +82,7 @@ function WalletDetailsForm({ onSubmit, buttonText }: WalletDetailsFormProps) {
           label="Wallet name"
           placeholder="My cool wallet"
           value={input.name}
-          error="testing"
+          error={error.name}
           onChange={onInputChange}
           required
         />
@@ -97,10 +106,15 @@ function WalletDetailsForm({ onSubmit, buttonText }: WalletDetailsFormProps) {
           onChange={onInputChange}
           required
         />
-        <Button onClick={() => onFormButtonClick()}>{buttonText}</Button>
+        <Stack direction="row">
+          <Button type="secondary">Cancel</Button>
+          {/* No idea how to make this button move all the way to the right */}
+          <Button onClick={() => onFormButtonClick()}>{buttonText}</Button>
+        </Stack>
       </Stack>
     </>
   );
 }
 
+export type { WalletDetailsFormProps };
 export default WalletDetailsForm;
