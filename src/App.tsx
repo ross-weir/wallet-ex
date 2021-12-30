3253d5de2c55b2eb0014545c494a34253ccece4c
ross-weir <29697678+ross-weir@users.noreply.github.com>
2021-12-30 18:16:39 +1100

    barebones again

--- src/App.tsx
+++ src/App.tsx
@@ -17,21 +17,22 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
-      <SensitiveModeProvider>
-        <React.StrictMode>
-          <Suspense fallback="loading">
-            <Routes>
-              <Route path="/" element={<Navigate to="/wallets" />} />
-              <Route path="/wallets" element={<WalletsList />} />
-              <Route path="/wallets/add" element={<AddWallet />} />
-              <Route path="/wallets/:walletId" element={<WalletView />} />
-              <Route path="*" element={<p>How did you get hur?</p>} />
-            </Routes>
-          </Suspense>
-        </React.StrictMode>
-      </SensitiveModeProvider>
-    </BrowserRouter>
+    <p>testoing....</p>
+    // <BrowserRouter>
+    //   <SensitiveModeProvider>
+    //     <React.StrictMode>
+    //       <Suspense fallback="loading">
+    //         <Routes>
+    //           <Route path="/" element={<Navigate to="/wallets" />} />
+    //           <Route path="/wallets" element={<WalletsList />} />
+    //           <Route path="/wallets/add" element={<AddWallet />} />
+    //           <Route path="/wallets/:walletId" element={<WalletView />} />
+    //           <Route path="*" element={<p>How did you get hur?</p>} />
+    //         </Routes>
+    //       </Suspense>
+    //     </React.StrictMode>
+    //   </SensitiveModeProvider>
+    // </BrowserRouter>
   );
 }
 
