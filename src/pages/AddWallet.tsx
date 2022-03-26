7b2c834a30188e99aca7df5797fae28a2b7cdd69
ross-weir <29697678+ross-weir@users.noreply.github.com>
2022-03-26 13:05:59 +1100

    update init view

--- src/pages/AddWallet.tsx
+++ src/pages/AddWallet.tsx
@@ -1,63 +1,19 @@
-import { Outlet, useLocation, useNavigate } from 'react-router';
-import { Breadcrumb,Container, Header } from 'semantic-ui-react';
+import { Outlet, useLocation } from 'react-router';
+import { Container, Header } from 'semantic-ui-react';
 
 import { capitalize } from '@/utils/fmt';
 
 function AddWallet() {
-  const navigate = useNavigate();
   const location = useLocation();
 
-  const Breadcrumbs = () => {
-    const parts = location.pathname.split('/');
-
-    // remove empty leading element
-    parts.shift();
-
-    return (
-      <Breadcrumb size="small">
-        {parts.map((path, idx) => {
-          const isLast = idx === parts.length - 1;
-          let href = '';
-          let onClick;
-
-          if (!isLast) {
-            href += '/';
-            href += parts
-              .reduce((prev: string[], curr: string, i) => {
-                if (i > idx) {
-                  return prev;
-                }
-
-                prev.push(curr);
-                return prev;
-              }, [])
-              .join('/');
-
-            onClick = () => navigate(href);
-          }
-
-          return (
-            <>
-              <Breadcrumb.Section
-                link={!isLast}
-                active={isLast}
-                onClick={onClick}
-              >
-                {capitalize(path)}
-              </Breadcrumb.Section>
-              {!isLast && <Breadcrumb.Divider />}
-            </>
-          );
-        })}
-      </Breadcrumb>
-    );
-  };
+  const urlParts = location.pathname.split('/');
+  // This will be either 'Add' | 'Create' | 'Restore'
+  const action = capitalize(urlParts[urlParts.length - 1]);
 
   return (
     <>
       <Container style={{ marginTop: 30 }}>
-        <Header as="h2" content="Add Wallet" />
-        {Breadcrumbs()}
+        <Header as="h2" content={`${action} Wallet`} />
         <Outlet />
       </Container>
     </>
